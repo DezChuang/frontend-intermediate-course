b9a480a321541b39ef33eadb7849cb55bd22e75b
dez_chuang <Dez_Chuang@compal.com>
2017-04-18 11:04:32 +0800

    [HW4][JS][Mod]Let jQuery render HTML tag

--- answers/hw4/public/js/main.js
+++ answers/hw4/public/js/main.js
@@ -3,54 +3,32 @@ var myContentType = 'application/vnd.twitchtv.v5+json';
 var twitchAPI = 'https://api.twitch.tv/kraken/streams/?game=League%20of%20Legends';
 
 $(document).ready(function () {
-  // $('#get-data').click(function () {
-
-    $.getJSON(twitchAPI, {client_id: clientID}, function (data) {
-
-      var items = $();
+  $.ajax({
+    url: twitchAPI,
+    headers: {'Client-ID': clientID},
+    type: 'GET',
+    contentType: myContentType,
+    success: function(data) {
       for(x = 0; x < 20; x++) {
-
-          //thumbnail
-          items = $();
-          items = items.add($( "<img>" ).attr( "src", data.streams[x].preview.medium).addClass('thumbnail') );
-          $('#stream-item-' + x).append( items );
-
-          //avatar
-          items = $();
-          items = items.add($( "<img>" ).attr( "src", data.streams[x].channel.logo).addClass('avatar') );
-          $('#stream-item-' + x).append( items );
-
-          //content
-          $('#stream-item-' + x).append( "<div class=\"content\">" +
-                                            "<p class=\"title\">" + data.streams[x].channel.status + "</p>" +
-                                            "<p class=\"name\">" + data.streams[x].channel.display_name + "</p>" +
-                                          "</div>" );
+        var $content =
+            "<div class='stream-item'>" +
+              "<img class='thumbnail' src='" + data.streams[x].preview.medium + "'>" +
+                "<div class='meta'>" +
+                  "<img class='avatar' src='" + data.streams[x].channel.logo + "'>" +
+                  "<div class='content'>" +
+                    "<p class='title'>" + data.streams[x].channel.status + "</p>" +
+                    "<p class='name'>" + data.streams[x].channel.display_name + "</p>" +
+                  "</div>"
+                "</div>" +
+              "</div>" +
+            "</div>";
+        $( ".container" ).append($content);
+        $content = $();
       }
-    })
-    .done(function() {
-      console.log( "second success" );
-    })
-    .fail(function() {
-      console.log( "error" );
-    })
-    .always(function() {
-      console.log( "complete" );
-    });
-  // });
+    },
+    error: function(data) {
+      console.log('error', data);
+    }
+  });
 });
-
-// $("#leader-btn").click(function() {
-//   $.ajax({
-//     url: twitchAPI,
-//     headers: {'Client-ID': clientID},
-//     type: 'GET',
-//     contentType: myContentType,
-//     success: function(data) {
-//       console.log(data);
-//     },
-//     error: function(data) {
-//       console.log('error', data);
-//     }
-//   });
-// });
 

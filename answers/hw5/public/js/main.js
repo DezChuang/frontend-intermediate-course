32f2d87a60e029d9f8f423ccb43a4c9b6bca2223
dez_chuang <Dez_Chuang@compal.com>
2017-05-16 16:43:45 +0800

    [HW5][JS][Mod]Refactor js as modulization version

--- answers/hw5/public/js/main.js
+++ answers/hw5/public/js/main.js
@@ -1,33 +1,58 @@
-var clientID = '5rdlf3sosdxy8kjprfx9lebgznkncf';
-var myContentType = 'application/vnd.twitchtv.v5+json';
-var twitchAPI = 'https://api.twitch.tv/kraken/streams/?game=League%20of%20Legends';
-
-$(document).ready(function () {
-  $.ajax({
-    url: twitchAPI,
-    headers: {'Client-ID': clientID},
-    type: 'GET',
-    contentType: myContentType,
-    success: function(data) {
-      for(x = 0; x < 20; x++) {
-        var $content =
-        `<div class='stream-item'>
-          <img class='preview' src="${data.streams[x].preview.medium}">
-          <div class='content'>
-            <img class='avatar' src="${data.streams[x].channel.logo}">
-            <div class='stream-text'>
-              <p class='title'>${data.streams[x].channel.status}</p>
-              <p class='streamer'>${data.streams[x].channel.display_name}</p>
-            </div>
-          </div>
-        </div>`;
-        $( ".container" ).append($content);
-        $content = $();
+$(document).ready(function(){
+  loadDataFromAPI((err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+      //ES6 syntax for `const streams = data.streams;`
+      const {streams} = data;
+      const $container = $('.container');
+      for(const stream of streams) {
+        $container.append(appendDataToDOM(stream));
       }
-    },
-    error: function(data) {
-      console.log('error', data);
     }
   });
 });
 
+function loadDataFromAPI(callback){
+  const clientId = '5rdlf3sosdxy8kjprfx9lebgznkncf';
+  const myContentType = 'application/vnd.twitchtv.v5+json';
+  const twitchApi = 'https://api.twitch.tv/kraken/streams/?game=League%20of%20Legends';
+  const game = 'League of Legends';
+  $.ajax({
+    type: 'GET',
+    url: twitchApi,
+    data: {
+      contentType: myContentType,
+      client_id: clientId,
+      game: game,
+      limit: 20
+    },
+    success: (data) => {
+      callback(null, data);
+    },
+    error: (err) => {
+      console.log(err);
+      callback(err);
+    }
+  });
+}
+
+function appendDataToDOM(data) {
+  var avatarSrc = data.channel.logo;
+  var previewSrc = data.preview.medium;
+  if(avatarSrc == null){
+    avatarSrc = "public/img/avt-default.png";
+  }
+  return (
+    `<div class='stream-item'>
+      <img class='preview' src="${previewSrc}">
+      <div class='content'>
+        <img class='avatar' src="${avatarSrc}">
+        <div class='stream-text'>
+          <p class='title'>${data.channel.status}</p>
+          <p class='streamer'>${data.channel.display_name}</p>
+        </div>
+      </div>
+    </div>`
+  );
+}

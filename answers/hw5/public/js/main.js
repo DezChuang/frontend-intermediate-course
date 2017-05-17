8d741d289780b62e4e79a02609116f64db6ef6cf
dez_chuang <Dez_Chuang@compal.com>
2017-05-17 13:38:41 +0800

    [HW5][JS][Mod]Finish hw5 js part

--- answers/hw5/public/js/main.js
+++ answers/hw5/public/js/main.js
@@ -1,23 +1,11 @@
-$(document).ready(function(){
-  loadDataFromAPI((err, data) => {
-    if (err) {
-        console.log(err);
-    } else {
-      //ES6 syntax for `const streams = data.streams;`
-      const {streams} = data;
-      const $container = $('.container');
-      for(const stream of streams) {
-        $container.append(appendDataToDOM(stream));
-      }
-    }
-  });
-});
+let offset = 0;
 
 function loadDataFromAPI(callback){
   const clientId = '5rdlf3sosdxy8kjprfx9lebgznkncf';
   const myContentType = 'application/vnd.twitchtv.v5+json';
-  const twitchApi = 'https://api.twitch.tv/kraken/streams/?game=League%20of%20Legends';
+  const twitchApi = 'https://api.twitch.tv/kraken/streams/';
   const game = 'League of Legends';
+  const limit = 15;
   $.ajax({
     type: 'GET',
     url: twitchApi,
@@ -25,7 +13,8 @@ function loadDataFromAPI(callback){
       contentType: myContentType,
       client_id: clientId,
       game: game,
-      limit: 20
+      limit: limit,
+      offset: offset
     },
     success: (data) => {
       callback(null, data);
@@ -37,22 +26,63 @@ function loadDataFromAPI(callback){
   });
 }
 
-function appendDataToDOM(data) {
+function templateData(data) {
   var avatarSrc = data.channel.logo;
   var previewSrc = data.preview.medium;
   if(avatarSrc == null){
     avatarSrc = "public/img/avt-default.png";
   }
   return (
-    `<div class='stream-item'>
-      <img class='preview' src="${previewSrc}">
-      <div class='content'>
-        <img class='avatar' src="${avatarSrc}">
-        <div class='stream-text'>
-          <p class='title'>${data.channel.status}</p>
-          <p class='streamer'>${data.channel.display_name}</p>
+    `<div class="stream-item">
+      <div class="preview">
+        <img src="${previewSrc}" onload="this.style.opacity=1;">
+      </div>
+      <div class="content">
+        <div class="avatar">
+          <img src="${avatarSrc}" onload="this.style.opacity=1;">
+        </div>
+        <div class="stream-text">
+          <p class="title">${data.channel.status}</p>
+          <p class="streamer">${data.channel.display_name}</p>
         </div>
       </div>
     </div>`
   );
 }
+
+function appendData() {
+  loadDataFromAPI((err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+      //ES6 syntax for `const streams = data.streams;`
+      const {streams} = data;
+      const $container = $('.container');
+      for(const stream of streams) {
+        $container.append(templateData(stream));
+      }
+      offset += 15;
+    }
+  });
+}
+
+// main function
+$(document).ready(function(){
+  // Init 20 items from twitch API
+  appendData();
+  // Infinite scroll
+  let timer;
+  const reservedHeight = 100;
+  const timeout = 250;
+  $(window).scroll(function() {
+    if(timer) {
+      window.clearTimeout(timer);
+    }
+    timer = window.setTimeout(function() {
+      if($(window).scrollTop() + $(window).height() > $(document).height() - reservedHeight) {
+          appendData();
+        // console.log($(".stream-item").length);
+      }
+    }, timeout);
+  });
+});
